4467cbd0b6dc793559166a244da56feb058d325d
tanouno <56457914+tanouno@users.noreply.github.com>
2019-12-10 23:01:13 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,55 @@
+enum Fruits {
+    //% block="Banana"
+    Banana,
+    //% block="Mela"
+    Apple,
+    //% block="Pesca"
+    Peach,
+    //% block="Limone"
+    Lemon
+}
+
+//% color="#AA278D"
+namespace basic {
+
+    //% block="Scegli la frutta %Friuts"
+    export function turn(choice: Fruits) {
+        switch(choice) {
+            case Banana:
+                basic.showLeds(`
+                    . . # . .
+                    . # . . .
+                    . # . . .
+                    . # . . .
+                    . . # . .
+                    `)break;
+            
+            case Apple:
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    `)break;
+            case Peach:
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . . . #
+                    # . . . #
+                    . # # # .
+                    `)break;
+            case Lemon:
+                basic.showLeds(`
+                    . . . . .
+                    . # # # .
+                    # . . . #
+                    # . . . #
+                    . # # # .
+                    `)break;    
+                break;
+        }
+
+    }
+}
